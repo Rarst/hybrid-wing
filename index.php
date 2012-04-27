ab78d7acd7246f7e5a45b3a96163a2926fb7491f
Rarst <contact@rarst.net>
2012-04-27 21:57:25 +0300

    Fixed deprecated hybrid_get_textdomain() calls.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 				<?php do_atomic( 'before_entry' ); ?>
 
 				<div class="entry-content">
-					<?php the_content( sprintf( __( 'Continue reading %1$s', hybrid_get_textdomain() ), the_title( ' "', '"', false ) ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<p class="page-links pages">' . __( 'Pages:', hybrid_get_textdomain() ), 'after' => '</p>' ) ); ?>
+					<?php the_content( sprintf( __( 'Continue reading %1$s', hybrid_get_parent_textdomain() ), the_title( ' "', '"', false ) ) ); ?>
+					<?php wp_link_pages( array( 'before' => '<p class="page-links pages">' . __( 'Pages:', hybrid_get_parent_textdomain() ), 'after' => '</p>' ) ); ?>
 				</div><!-- .entry-content -->
 
 				<?php do_atomic( 'after_entry' ); ?>
