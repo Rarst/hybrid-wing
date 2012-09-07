8a844a74dce875ebff4c7bc2c22c1944ce948b50
Rarst <contact@rarst.net>
2012-09-07 16:56:18 +0300

    Moved non-template files to subdirectory.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once TEMPLATEPATH . '/hybrid-core/hybrid.php';
-require TEMPLATEPATH . '/class-hybrid-wing.php';
+require TEMPLATEPATH . '/php/class-hybrid-wing.php';
 
 $hybrid_wing = new Hybrid_Wing();
