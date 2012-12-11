bd519422ca18b10e164fb7904dd432495a27ffcf
Rarst <contact@rarst.net>
2012-12-11 16:47:54 +0200

    Updated charset declaration in head.

--- header.php
+++ header.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php hybrid_document_title(); ?></title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php hybrid_document_title(); ?></title>
 
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<?php wp_head(); // wp_head ?>
-
+	<?php wp_head(); ?>
 </head>
 
 <body class="<?php hybrid_body_class(); ?>">
