2575430ae9f1e360001c57aadf2e7c131dbaa47c
Andrey Savchenko <contact@rarst.net>
2014-06-20 23:59:08 +0300

    Removed remainders of navbar sidebar.

--- php/class-navbar.php
+++ php/class-navbar.php
@@ -13,10 +13,6 @@ class Navbar {
 	function __construct() {
 
 		add_action( 'init', array( $this, 'init' ) );
-
-//		if ( $this->args->sidebar ) {
-//			add_action( 'widgets_init', array( $this, 'widgets_init' ) );
-//		}
 	}
 
 	function init() {
@@ -24,24 +20,6 @@ class Navbar {
 		register_nav_menu( 'navbar', 'Navbar' );
 
 		add_filter( 'wp_nav_menu_args', array( $this, 'wp_nav_menu_args' ), 9 );
-	}
-
-	function widgets_init() {
-
-		global $wp_registered_sidebars;
-
-		if ( empty( $wp_registered_sidebars[$this->args->name] ) ) {
-			register_sidebar(
-				array(
-					'name'          => 'Navbar',
-					'id'            => $this->args->name,
-					'before_widget' => '<li id="%1$s" class="dropdown widget %2$s">',
-					'before_title'  => '<a href="#" class="dropdown-toggle" data-toggle="dropdown">',
-					'after_title'   => '<b class="caret"></b></a><div class="dropdown-menu">',
-					'after_widget'  => '</div></li>',
-				)
-			);
-		}
 	}
 
 	/**
--- sidebar-navbar.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'navbar' ) ) : ?>
-
-<ul class="nav">
-
-	<?php dynamic_sidebar( 'navbar' ); ?>
-
-</ul>
-
-<?php endif; ?>
